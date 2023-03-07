fd15f93919f7895fdd52e113d71540220ca904d1
Krishna Pradhan <kpaccess@gmail.com>
2023-03-07 08:10:51 -0500

    fixed page height in experience page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
           </p>
           <div>
             <Link
-              href={`\${portfolio}`}
+              href="/portfolio"
               className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
             >
               Portfolio
