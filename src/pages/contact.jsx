9e65e3f2b1b5af4692cb19ca94d51e77a210861d
Krishna Pradhan <kpaccess@gmail.com>
2023-03-07 09:57:21 -0500

    fixed failed build for apostrophe

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Contact = () => {
   return (
     <div
@@ -38,7 +36,7 @@ const Contact = () => {
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             ></textarea>
             <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto rounded-md flex items-center hover:scale-110 duration-300">
-              Let's talk
+              Let&apos;s talk
             </button>
           </form>
         </div>
